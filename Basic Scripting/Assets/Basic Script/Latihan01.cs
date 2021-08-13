3016d35d040726fb0d4bb3d7aca9c6faf2c6811a
AnggaKusumaAdy <88541564+AnggaKusumaAdy@users.noreply.github.com>
2021-08-13 14:35:07 +0700

    Latihan Basic Script
    Basic Script

---
+++ Basic Scripting/Assets/Basic Script/Latihan01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Selamat Sore, Ini latihan 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
