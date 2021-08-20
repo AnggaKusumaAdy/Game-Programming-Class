49e354d3fc087d9cc73646d53cdd459bf089e74f
AnggaKusumaAdy <88541564+AnggaKusumaAdy@users.noreply.github.com>
2021-08-20 14:26:08 +0700

    Pertemuan Lalu
    Kurang latihan 2 dan 3 karena tidak tau jika ada latihan 2 dan 3

---
+++ Basic Scripting/Assets/Latihan 2/Latihan2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour
+{
+    public int A;
+    public int B;
+    [Range(1, 10)]
+    public int C;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int total = (A + B) * C;
+        Debug.Log("Total: " + total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
